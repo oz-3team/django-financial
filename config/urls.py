415a3d4c11cf7597e9b0be1670c7f72b290e74fe
monda95 <jungmonda@naver.com>
2025-08-27 21:09:22 +0900

    analysis url수정

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.analysis import urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/notifications/', include('apps.notification.urls')),
-    path('api/analysis/', include('analysis.urls')),
+    path('api/analysis/', include('apps.analysis.urls')),
 ]
