f841b6f502bcf8290b52b7c9f56809acd235b6d2
monda95 <jungmonda@naver.com>
2025-08-28 18:44:15 +0900

    ✨ Media_root 설정 추가, analysis/signal 추가
    result image 2025/08/30/.jpg 식으로 저장되게 구현, signals.py 추가해서 글 삭제시 db와 동적저장소에서도 자동  제거되도록 함

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent 
 
 load_dotenv()
 
@@ -128,8 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+# STATIC & MEDIA
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
