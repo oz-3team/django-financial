e8ec69a8414963c44822ccaf58563d54766da0d6
monda95 <jungmonda@naver.com>
2025-08-28 20:53:32 +0900

    🐛 Fixed db name

--- config/settings/base.py
+++ config/settings/base.py
@@ -122,7 +122,7 @@ DATABASES = {
         'NAME': os.environ.get("POSTGRES_DB", "financial_db"),
         'USER': os.environ.get("POSTGRES_USER", "financial_user"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "1234"),
-        'HOST': os.environ.get("POSTGRES_HOST", "localhost"),
+        'HOST': os.environ.get("POSTGRES_HOST", "db"),
         'PORT': os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
