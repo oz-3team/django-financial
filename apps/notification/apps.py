bdeadda32291f77f38ab1d3a21ab71a5235565f3
kmg171 <qweaiiok4781@gmail.com>
2025-08-30 18:12:02 +0900

    작업중인 내용 임시 커밋

--- apps/notification/apps.py
+++ apps/notification/apps.py
@@ -6,4 +6,5 @@ class NotificationConfig(AppConfig):
     name = "apps.notification"
 
     def ready(self):
-        pass
+        # 앱이 준비될 때 signals 모듈을 임포트하여 시그널 등록
+        import apps.notification.signals  # noqa: F401
